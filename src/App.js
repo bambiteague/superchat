35d4792ca5c4e17fed0abbe00de3af2d6f4f53d3
bambi teague <teeegsa@gmail.com>
2021-10-18 11:30:10 -0500

    adding needed hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {userCollectionData} from 'react-firebase-hooks/firestore';
 function App() {
   return (
     <div className="App">
